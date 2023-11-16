8f0c5cf768aeca135326230bc3779b81c0b9b4f8
akshaic <147908616+akshaic@users.noreply.github.com>
2023-11-16 11:51:08 +0530

    sample program to show abstract class

--- abstractcl.java
+++ abstractcl.java
@@ -1,8 +1,7 @@
 import java.util.*;
 abstract class Shape
 {
-abstract void noofsides()
-{}
+abstract void noofsides();
 }
 class Triangle extends Shape
 {
@@ -27,5 +26,13 @@ System.out.println("Six sides");
 }
 class abstractcl
 {
-
-
+public static void main(String args[])
+{
+Shape ob1=new Rectangle();
+ob1.noofsides();
+Shape ob2=new Triangle();
+ob2.noofsides();
+Shape ob3=new Hexagon();
+ob3.noofsides();
+}
+}
